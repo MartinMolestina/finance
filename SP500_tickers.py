951d87f01f0f59d0db493cc47360087111887c48
MartinMolestina <martin.molestina@gmail.com>
2019-11-05 20:04:40 +0100

    created joined csv on adjclose columns with tickers

--- SP500_tickers.py
+++ SP500_tickers.py
@@ -49,5 +49,38 @@ def get_data_from_yahoo(reload_sp500=False):
         except:
             pass
 
-save_sp500_tickers()
-get_data_from_yahoo()
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+    errors = 0
+    
+    for count, ticker in enumerate(tickers):
+        try:
+            df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+            df.set_index('formatted_date', inplace=True)
+
+            df.rename(columns={'adjclose': ticker}, inplace=True)
+            df.drop(['date','index','close','high','low','open','volume'], 1, inplace=True)
+
+            if main_df.empty:
+                main_df = df
+            else:
+                main_df = main_df.join(df, how='outer')
+
+            if count % 10 == 0:
+                print(count)
+        except:
+            errors += 1
+            pass
+
+    print(main_df.tail())
+    print('Errors: {}'.format(errors))
+    main_df.to_csv('sp500_joined_closes.csv')
+
+
+
+#save_sp500_tickers()
+#get_data_from_yahoo()
+compile_data()
